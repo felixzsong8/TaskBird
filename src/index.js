4710c127b3104b59713d0853ab00baf6c618b8ae
aobu <56887587+aobu@users.noreply.github.com>
2023-12-06 16:32:44 -0500

    Add files via upload
    CALENDAR NODEJS APP. NO MODULES

---
+++ src/index.js
@@ -0,0 +1,211 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import { google } from 'googleapis';
+import dayjs from 'dayjs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import session from 'express-session';
+import moment from 'moment-timezone';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Google Calendar API setup
+const calendar = google.calendar('v3');
+
+// OAuth2 client setup
+const oauth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URL
+);
+
+// Session setup
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // Set to true if using https
+}));
+
+// Middleware to check if the user is authenticated
+const isAuthenticated = (req, res, next) => {
+    if (req.session.tokens) {
+        oauth2Client.setCredentials(req.session.tokens);
+        next();
+    } else {
+        res.redirect('/google');
+    }
+};
+
+// Middleware to parse JSON body
+app.use(express.json());
+
+// Serve static files
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Google OAuth2 authentication URL
+app.get('/google', (req, res) => {
+    const url = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: ['https://www.googleapis.com/auth/calendar'],
+        prompt: 'consent'
+    });
+    res.redirect(url);
+});
+
+// Handling OAuth2 redirection
+app.get('/google/redirect', async (req, res) => {
+    const { code } = req.query;
+    try {
+        const { tokens } = await oauth2Client.getToken(code);
+        req.session.tokens = tokens; // Store tokens in session
+        res.redirect('/check_taskbird_calendar');
+    } catch (error) {
+        console.error('Error during login:', error);
+        res.status(500).send('Authentication failed');
+    }
+});
+
+// Check if TaskBird calendar exists or create it
+app.get('/check_taskbird_calendar', isAuthenticated, async (req, res) => {
+    try {
+        const calendars = await calendar.calendarList.list({ auth: oauth2Client });
+        let taskBirdCalendar = calendars.data.items.find(cal => cal.summary === 'TaskBird');
+
+        if (!taskBirdCalendar) {
+            const newCalendar = await calendar.calendars.insert({
+                auth: oauth2Client,
+                requestBody: {
+                    summary: 'TaskBird',
+                    description: 'Calendar for TaskBird app events',
+                    timeZone: 'America/New_York'
+                }
+            });
+            taskBirdCalendar = newCalendar.data;
+        }
+
+        req.session.taskBirdCalendarId = taskBirdCalendar.id; // Store TaskBird calendar ID in session
+        res.redirect('/frontend');
+    } catch (error) {
+        console.error('Error checking TaskBird calendar:', error);
+        res.status(500).send('Failed to check TaskBird calendar');
+    }
+});
+
+// Fetch events from TaskBird calendar for a specific month
+app.get('/get_events', isAuthenticated, async (req, res) => {
+    const { year, month } = req.query;
+    const timeMin = new Date(year, month, 1).toISOString();
+    const timeMax = new Date(year, parseInt(month) + 1, 0).toISOString();
+
+    try {
+        const { taskBirdCalendarId } = req.session;
+        const events = await calendar.events.list({
+            auth: oauth2Client,
+            calendarId: taskBirdCalendarId,
+            timeMin: timeMin,
+            timeMax: timeMax,
+            maxResults: 250, // Adjust as needed
+            singleEvents: true,
+            orderBy: 'startTime'
+        });
+        // Log the events to the console
+        //console.log(`Fetched ${events.data.items.length} events:`, events.data.items);
+        res.json(events.data.items);
+    } catch (error) {
+        console.error('Error fetching events:', error);
+        res.status(500).send('Failed to fetch events');
+    }
+});
+
+// Fetch events from TaskBird calendar for a specific day
+app.get('/get_events_day', isAuthenticated, async (req, res) => {
+    const { year, month, day } = req.query;
+    const timeMin = day ? new Date(year, month, day).toISOString() : new Date(year, month, 1).toISOString();
+    const timeMax = day ? new Date(year, month, parseInt(day) + 1).toISOString() : new Date(year, parseInt(month) + 1, 0).toISOString();
+
+    try {
+        const { taskBirdCalendarId } = req.session;
+        const events = await calendar.events.list({
+            auth: oauth2Client,
+            calendarId: taskBirdCalendarId,
+            timeMin: timeMin,
+            timeMax: timeMax,
+            maxResults: 250, // Adjust as needed
+            singleEvents: true,
+            orderBy: 'startTime'
+        });
+
+        res.json(events.data.items);
+    } catch (error) {
+        console.error('Error fetching events:', error);
+        res.status(500).send('Failed to fetch events');
+    }
+});
+
+// Endpoint to add an event to the Google Calendar
+app.post('/add_event', isAuthenticated, async (req, res) => {
+    console.log('Received request:', req.body);
+    const { summary, location, description, start, end } = req.body;
+
+    const formattedStartDateTime = new Date(start.dateTime).toISOString();
+    const formattedEndDateTime = new Date(end.dateTime).toISOString();
+
+    const event = {
+        summary,
+        location,
+        description,
+        start: { dateTime: formattedStartDateTime, timeZone: start.timeZone },
+        end: { dateTime: formattedEndDateTime, timeZone: end.timeZone }
+    };
+
+    try {
+        const { taskBirdCalendarId } = req.session; // Use the stored calendar ID from the session
+
+        const response = await calendar.events.insert({
+            calendarId: taskBirdCalendarId,
+            auth: oauth2Client,
+            resource: event
+        });
+
+        res.status(200).json(response.data);
+    } catch (error) {
+        console.error('Error adding event:', error);
+        res.status(500).send('Failed to add event');
+    }
+});
+
+// Endpoint to delete an event from the Google Calendar
+app.post('/delete_event', isAuthenticated, async (req, res) => {
+    const { eventId } = req.body;
+
+    try {
+        const { taskBirdCalendarId } = req.session; // Use the stored calendar ID from the session
+
+        const response = await calendar.events.delete({
+            calendarId: taskBirdCalendarId,
+            eventId: eventId,
+            auth: oauth2Client
+        });
+
+        res.status(200).json({ message: 'Event deleted successfully', deletedEvent: response.data });
+    } catch (error) {
+        console.error('Error deleting event:', error);
+        res.status(500).send('Failed to delete event');
+    }
+});
+
+// Serve HTML page with frontend JavaScript
+app.get('/frontend', isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server started on PORT ${PORT}`);
+});
---
+++ src/public/frontendscript.js
@@ -0,0 +1,177 @@
+const daysTag = document.querySelector(".days");
+const currentDate = document.querySelector(".current-date");
+const prevIcon = document.querySelector('.material-symbols-rounded:first-child');
+const nextIcon = document.querySelector('.material-symbols-rounded:last-child');
+
+let date = new Date();
+let currYear = date.getFullYear();
+let currMonth = date.getMonth();
+
+const months = ["January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"];
+
+async function fetchEventsForMonth(year, month) {
+    const response = await fetch(`/get_events?year=${year}&month=${month}`);
+    const events = await response.json();
+    return events.map(event => new Date(event.start.dateTime || event.start.date).getDate());
+}
+
+async function fetchEventsForDay(year, month, day) {
+    const response = await fetch(`/get_events_day?year=${year}&month=${month}&day=${day}`);
+    const events = await response.json();
+    return events;
+}
+
+const renderCalendar = async () => {
+    const firstDayOfMonth = new Date(currYear, currMonth, 1).getDay();
+    const lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate();
+    const lastDayOfMonth = new Date(currYear, currMonth, lastDateOfMonth).getDay();
+    const lastDateOfLastMonth = new Date(currYear, currMonth, 0).getDate();
+    let liTag = "";
+
+    const eventDates = await fetchEventsForMonth(currYear, currMonth);
+
+    for (let i = firstDayOfMonth; i > 0; i--) {
+        liTag += `<li class="inactive">${lastDateOfLastMonth - i + 1}</li>`;
+    }
+
+    for (let i = 1; i <= lastDateOfMonth; i++) {
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear() ? "active" : "";
+        let hasEvent = eventDates.includes(i);
+        let eventDot = hasEvent ? '<span class="event-dot"></span>' : '';
+        liTag += `<li class="${isToday}" data-day="${i}">${i}${eventDot}</li>`;
+    }
+
+    for (let i = lastDayOfMonth; i < 6; i++) {
+        liTag += `<li class="inactive">${i - lastDayOfMonth + 1}</li>`;
+    }
+    currentDate.innerText = `${months[currMonth]} ${currYear}`;
+    daysTag.innerHTML = liTag;
+    formattedCurrentDate = `${months[currMonth]} ${currYear}`;
+    const eventDetailsHeader = document.querySelector('.event-details h2');
+    eventDetailsHeader.innerText = `${formattedCurrentDate} Events`;
+
+    const dayElements = document.querySelectorAll('.days li');
+    dayElements.forEach((dayElement) => {
+        dayElement.addEventListener('click', handleDayClick);
+    });
+};
+
+function handleDayClick(event) {
+    const clickedDay = event.target.dataset.day;
+    const formattedDate = `${currYear}-${(currMonth + 1).toString().padStart(2, '0')}-${clickedDay.toString().padStart(2, '0')}`;
+
+    fetchEventsForDay(currYear, currMonth, clickedDay).then(events => {
+        const eventList = document.getElementById('eventList');
+        eventList.innerHTML = '';
+
+        events.forEach(event => {
+            const li = document.createElement('li');
+            li.textContent = `${event.summary} - ${new Date(event.start.dateTime).toLocaleTimeString()} to ${new Date(event.end.dateTime).toLocaleTimeString()}`;
+            
+            const deleteButton = createDeleteButton(event.id);
+            li.appendChild(deleteButton);
+            
+            // Append the list item to the event list
+            eventList.appendChild(li);
+        });
+    }).catch(error => {
+        console.error('Error fetching events for day:', error);
+    });
+}
+
+prevIcon.addEventListener('click', () => {
+    currMonth = currMonth - 1;
+    handleMonthChange();
+});
+
+nextIcon.addEventListener('click', () => {
+    currMonth = currMonth + 1;
+    handleMonthChange();
+});
+
+function handleMonthChange() {
+    if (currMonth < 0 || currMonth > 11) {
+        date = new Date(currYear, currMonth, new Date().getDate());
+        currYear = date.getFullYear();
+        currMonth = date.getMonth();
+    } else {
+        date = new Date();
+    }
+
+    renderCalendar();
+}
+
+function createDeleteButton(eventId) {
+    const button = document.createElement('button');
+    button.textContent = 'Delete';
+    button.eventId = eventId;
+    button.addEventListener('click', () => handleDeleteClick(button.eventId));
+    return button;
+}
+
+async function handleDeleteClick(eventId) {
+    try {
+        const response = await fetch('/delete_event', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ eventId })
+        });
+    
+        if (response.ok) {
+            const result = await response.json();
+            console.log('Event deleted:', result.deletedEvent);
+        } else {
+            console.error('Failed to delete event');
+        }
+    } catch (error) {
+        console.error('Error deleting event:', error);
+    }
+    window.location.reload();
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const eventForm = document.getElementById('eventForm');
+
+    eventForm.addEventListener('submit', async function(e) {
+        e.preventDefault();
+
+        const summary = document.getElementById('summary').value;
+        const location = document.getElementById('location').value;
+        const description = document.getElementById('description').value;
+        const startDateTime = document.getElementById('startDateTime').value;
+        const endDateTime = document.getElementById('endDateTime').value;
+
+        const event = {
+            summary,
+            location,
+            description,
+            start: { dateTime: startDateTime, timeZone: 'America/New_York' },
+            end: { dateTime: endDateTime, timeZone: 'America/New_York' }
+        };
+
+        try {
+            const response = await fetch('/add_event', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(event)
+            });
+
+            if (response.ok) {
+                const result = await response.json();
+                console.log('Event added:', result);
+                window.location.reload();
+            } else {
+                console.error('Failed to add event');
+            }
+        } catch (error) {
+            console.error('Error submitting event:', error);
+        }
+    });
+});
+renderCalendar();
